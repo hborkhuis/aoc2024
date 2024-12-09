a05d61ff5853ca4134a650a40bcc2bb5304699a1
Harry Borkhuis <harry.borkhuis@tauw.com>
2024-12-10 00:36:07 +0100

    B vraag nog niet goed

--- Dag2/Program.cs
+++ Dag2/Program.cs
@@ -18,6 +18,35 @@ foreach (var line in differntiatedLines)
     lineIndex++;
 }
 Console.WriteLine(safeItems);
+safeItems = 0;
+lineIndex = 0;
+foreach (var line in differntiatedLines)
+{
+    if(Safe(line))
+    {
+        safeItems++;
+        Console.WriteLine($"Safe line: linenum={lineIndex+1}: {string.Join(',', lines[lineIndex])}");
+    }
+    else if(TryFixLine(line, out int[] fixedLine))
+    {
+        if(Safe(fixedLine))
+        {
+            safeItems++;
+            Console.WriteLine($"Successfuly fixed line: linenum={lineIndex+1}: {string.Join(',', lines[lineIndex])}");
+        }
+        else
+        {
+            Console.WriteLine($"Fxed line not succesfull: linenum={lineIndex+1}: {string.Join(',', lines[lineIndex])}");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Could not fixed line: linenum={lineIndex+1}: {string.Join(',', lines[lineIndex])}");
+    }
+    lineIndex++;
+}
+Console.WriteLine(safeItems);
+
 
 static bool Safe(int[] line)
 {
@@ -26,6 +55,42 @@ static bool Safe(int[] line)
     // Check if at least (minus)one and all increasing or decreasing
     safe &= line.All(x => x <0) || line.All(x => x > 0);
     return safe;
+}
+
+static bool TryFixLine(int[] line, out int[] fixedLine)
+{
+    bool lineFixed = false;
+    List<int>fixedList = new List<int>();
+    int currDirrection = 0;
+    int skippedDifference = 0;
+    int nrSkipped = 0;
+    for(int i = 0; i < line.Length; i++)
+    {
+        bool skip = line[i] == 0 || line[i]*currDirrection < 0 || Math.Abs(line[i]) > 3;
+        if (skip) {
+            // Remember the skipped one, add it to the next (only if not the first level in line)
+            if(i>0)
+                skippedDifference = line[i];
+            nrSkipped++;
+        }
+        else
+        {
+            fixedList.Add(line[i] + skippedDifference);
+            skippedDifference = 0;
+            currDirrection = line[i]/Math.Abs(line[i]);
+        }
+    }
+    if(nrSkipped <2)
+    {
+        lineFixed = true;
+        fixedLine = fixedList.ToArray();
+        Console.WriteLine($"Line {string.Join(',', line)} fixed to {string.Join(',', fixedLine)}");
+    }
+    else {
+        // nothing done
+        fixedLine = line.ToArray();
+    }
+    return lineFixed;
 }
 
 static void FillLists(out List<int[]> list)
